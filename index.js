39110cb0ab2a3524d2fcf0ab1874686fe68034f3
William Nicholson <will98nicholson@gmail.com>
2021-03-22 16:18:13 -0400

    questions

--- index.js
+++ index.js
@@ -3,7 +3,39 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of this project?"
+    }
+
+    {
+        type: "input",
+        name: "Description",
+        message: "Please provide a description of your project."
+    }
+    {
+        type: "input",
+        name: "Installation Instructions",
+        message: "Please provide instructions for how this application is to be installed."
+    }
+    {
+        type: "input",
+        name: "Usage Information",
+        message: "Please provide information on how users should be using this application."
+    }
+    {
+        type: "input",
+        name: "Contribution Guidelines",
+        message: "Please provide any contribution guidelines you wish to be included"
+    }
+    {
+        type: "input",
+        name: "Test Instructions",
+        message: "Please provide test instructions for your application."
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
